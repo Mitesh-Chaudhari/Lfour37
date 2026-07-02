958f1769ee652183b8546832718af99831133eee
Mitesh Chaudhari <miteshchaudhari1999@gmail.com>
2026-07-02 21:44:45 +0530

    product detail scroll issue fix

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from '@/components/layout/navbar'
 import { Footer } from '@/components/layout/footer'
 import { CartDrawer } from '@/components/cart/cart-drawer'
+import { ScrollToTop } from '@/components/layout/scroll-to-top'
 
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <CartDrawer />
       <main className="min-h-screen">{children}</main>
